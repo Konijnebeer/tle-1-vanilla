969924f418b1886cbae81299e521ffbf608ba1fa
RexTheWeeb <D.F.vantwoudt@outlook.com>
2025-09-22 13:36:37 +0200

    Home Start.

---
+++ api/home.php
@@ -0,0 +1,42 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="styles/output.css" rel="stylesheet">
+    <title>Home</title>
+</head>
+<body>
+<header>
+    <h1>App Name</h1>
+</header>
+<main>
+    <div class="bg-gray-400">
+        <h2>Contacts</h2>
+    </div>
+    <div class="bg-gray-400">
+        <h2>Account</h2>
+    </div>
+    <div class="bg-gray-400">
+        <h2>Discover</h2>
+    </div>
+    <div class="bg-gray-400">
+        <h2>History</h2>
+    </div>
+    <div>
+        <h2>Service</h2>
+    </div>
+</main>
+
+<footer>
+    <p>Settings</p>
+</footer>
+
+</body>
+</html>
